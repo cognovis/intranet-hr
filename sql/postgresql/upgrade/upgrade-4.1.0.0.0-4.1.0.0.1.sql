2ed1c4bc67b36f6af0bbdcc73015ca23b833c885
Malte Sussdorff <malte.sussdorff@cognovis.de>
2014-12-15 13:01:55 +0100

    Ensured we are in the right package

--- sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql
+++ sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql
@@ -1,4 +1,4 @@
-SELECT acs_log__debug('/packages/intranet-timesheet2/sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql','');
+SELECT acs_log__debug('/packages/intranet-hr/sql/postgresql/upgrade/upgrade-4.1.0.0.0-4.1.0.0.1.sql','');
 
 -- -------------------------------------------------------
 -- Create new Absence types for weekends
